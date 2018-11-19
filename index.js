01d8b883398dd5d4dc74cddf9b67214c4926ffd4
Sean Hendry <sean.c.hendry@gmail.com>
2018-11-19 16:15:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string)
+  if (string.toLowerCase() === string){
+    return '';
+  }
+  if string.toUpperCase( === string){
+    return '';
+  }
 }
 
