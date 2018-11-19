10729f9aaca1af1e6d9da2f44c4bb4a630dca993
Sean Hendry <sean.c.hendry@gmail.com>
2018-11-19 15:56:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
-    expect(shout('hello')).toEqual('HELLO')
-  })
-})
-
-describe('whisper(string)', function() {
-  it('receives one argument and returns it in all lowercase', function() {
-    expect(whisper('HELLO')).toEqual('hello')
-  })
-})
-
+function shout(string) {
+  return string.toUpperCase()
+}
